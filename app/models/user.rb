f44f369ba7abe08715a800229fb07403d4d2f2fa
Yuri Mikhaylov <me@yurijmi.ru>
2020-02-08 17:09:54 +0300

    Add UsersController with create action
    Signed-off-by: Yuri Mikhaylov <me@yurijmi.ru>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_secure_password
   
   validates :email, presence: true, uniqueness: true
+  
+  before_save :downcase_fields
+  
+private
+  
+  def downcase_fields
+    email.downcase!
+  end
 end
