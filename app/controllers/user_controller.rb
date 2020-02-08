4dc4e4e9b67e888b4859da42c7cfd283878461aa
Yuri Mikhaylov <me@yurijmi.ru>
2020-02-08 17:45:22 +0300

    Rename UsersController and add show action for current_user
    Signed-off-by: Yuri Mikhaylov <me@yurijmi.ru>

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,18 @@
-class UsersController < ApplicationController
+class UserController < ApplicationController
+  before_action :authenticate_user, only: :show
+  
   def create
     @user = User.new(user_params)
     
     if @user.save
-      render json: @user, status: :created
+      render json: @user, status: :created, location: user_url
     else
       render json: @user.errors, status: :unprocessable_entity
     end
+  end
+  
+  def show
+    render json: current_user
   end
   
 private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: :create
+  get  'user'       => 'user#show'
+  post 'user'       => 'user#create'
   post 'user_token' => 'user_token#create'
 end
