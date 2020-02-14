484feb888bc0f71829ac8486780d282779c6ecf1
Yuri Mikhaylov <me@yurijmi.ru>
2020-02-14 15:08:00 +0300

    Fix association in SelectedItem
    Signed-off-by: Yuri Mikhaylov <me@yurijmi.ru>

--- app/models/selected_item.rb
+++ app/models/selected_item.rb
@@ -1,11 +1,11 @@
 class SelectedItem < ApplicationRecord
-  belongs_to :accessory_item
+  belongs_to :item, class_name: 'AccessoryItem', foreign_key: 'accessory_item_id'
+  
   belongs_to :cart,  optional: true
   belongs_to :order, optional: true
   
-  validates :accessory_item, presence: true
-  validates :price_cents,    presence: true, numericality: { greater_than: 0 }
-  validates :quantity,       presence: true, numericality: { greater_than: 0 }
+  validates :price_cents, presence: true, numericality: { greater_than: 0 }
+  validates :quantity,    presence: true, numericality: { greater_than: 0 }
   
   validate :cart_id_xor_order_id
   
