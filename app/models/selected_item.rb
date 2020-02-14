1de23a83182e608a86031a40cef70c027fb73f0d
Yuri Mikhaylov <me@yurijmi.ru>
2020-02-14 14:59:43 +0300

    Add more validations to SelectedItem model
    Signed-off-by: Yuri Mikhaylov <me@yurijmi.ru>

--- app/models/selected_item.rb
+++ app/models/selected_item.rb
@@ -3,6 +3,10 @@ class SelectedItem < ApplicationRecord
   belongs_to :cart,  optional: true
   belongs_to :order, optional: true
   
+  validates :accessory_item, presence: true
+  validates :price_cents,    presence: true, numericality: { greater_than: 0 }
+  validates :quantity,       presence: true, numericality: { greater_than: 0 }
+  
   validate :cart_id_xor_order_id
   
 private
