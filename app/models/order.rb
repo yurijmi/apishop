dbfb6273d12ee2cc8fdf2321c05f3a181549dc06
Yuri Mikhaylov <me@yurijmi.ru>
2020-02-17 11:12:55 +0300

    Update Order. Add validations, enum and from_cart method
    Signed-off-by: Yuri Mikhaylov <me@yurijmi.ru>

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,28 @@ class Order < ApplicationRecord
   
   has_many :selected_items, dependent: :destroy, counter_cache: :items_count
   has_many :items, through: :selected_items, class_name: 'AccessoryItem'
+  
+  enum status: [:pending, :shipping, :completed]
+  
+  validates :address, presence: true
+  
+  # Creates an Order from existing Cart
+  # 
+  # @param [Cart] cart Cart to create order from
+  # @param [Hash] params Parameters for the Order
+  # 
+  # @return [Order] New Order object
+  def self.from_cart(cart, params = {})
+    order = Order.new(params)
+    order.user_id = cart.user_id
+    
+    cart.selected_items.each do |item|
+      order.selected_items << item
+      order.total_cents += item.price_cents
+      
+      item.cart_id = nil
+    end
+    
+    order
+  end
 end
