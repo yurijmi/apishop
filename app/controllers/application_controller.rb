87bcadc4bda3b26120e4f88fa8c17ed48d4b84d5
Yuri Mikhaylov <me@yurijmi.ru>
2020-02-17 14:04:15 +0300

    Setup Pundit
    Signed-off-by: Yuri Mikhaylov <me@yurijmi.ru>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
+  include Pundit
+  
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
+private
+  
+  def user_not_authorized
+    render json: { error: 'you are not authorized for this action' }, status: :forbidden
+  end
 end
