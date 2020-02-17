aa7c67184b292409956452969cb94a363d4f0d82
Yuri Mikhaylov <me@yurijmi.ru>
2020-02-17 13:54:37 +0300

    Add admin field to Users
    Signed-off-by: Yuri Mikhaylov <me@yurijmi.ru>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_112308) do
+ActiveRecord::Schema.define(version: 2020_02_17_105338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2020_02_14_112308) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
